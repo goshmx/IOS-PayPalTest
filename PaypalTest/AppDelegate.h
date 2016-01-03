1198e092945654a2369fdfd78d17a844c553e0bb
TRON <osvaldocube@gmail.com>
2016-01-03 04:04:01 -0600

    Initial Commit

---
+++ PaypalTest/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PaypalTest
+//
+//  Created by TRON on 03/01/16.
+//  Copyright © 2016 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ PaypalTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PaypalTest
+//
+//  Created by TRON on 03/01/16.
+//  Copyright © 2016 TRON. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
